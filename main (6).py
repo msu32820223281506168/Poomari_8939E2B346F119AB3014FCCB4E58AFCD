2c7bc430647eed2736959340d8b4a931f0e1035a
msu32820223281506168 <144206318+msu32820223281506168@users.noreply.github.com>
2023-09-22 09:59:53 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,14 @@
+def linear_search_product(products, target):
+    indices = []
+    
+    for i in range(len(products)):
+        if products[i] == target:
+            indices.append(i)
+    
+    return indices
+products = ["apple", "banana", "apple", "grapes", "apple"]
+target = "apple"
+print(linear_search_product(products, target))
+
+#Output:
+#[0, 2,4]
